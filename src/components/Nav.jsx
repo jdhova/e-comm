7905b6d62c26d006b4f958e507136a16505636c9
Jude Okagu <juud0010@gmail.com>
2022-07-05 19:27:55 -0400

    done components

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from 'react';
+import {Link } from 'react-router-dom'
+import { Container,Navbar } from 'react-bootstrap';
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <div>
+        
+<Navbar className='nav'>
+  <Container className='navhome'>
+    <Navbar.Brand><Link to="/">Home</Link></Navbar.Brand>
+    <Navbar.Toggle />
+    <Navbar.Collapse className="justify-content-end"> 
+      <Navbar.Text>
+          <ul className='links'>
+            <li> <Link to="/Signin">Signin</Link></li>
+            <li> <Link to="/Signout">Signout</Link></li>
+            
+          </ul>
+      </Navbar.Text>
+    </Navbar.Collapse>
+  </Container>
+</Navbar>
+       
+    </div>
   )
 }
 
 export default Nav
+
