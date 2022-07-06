ed9de1927d799feed009e270f2e6cab1c43a7d70
Jude Okagu <juud0010@gmail.com>
2022-07-05 20:44:46 -0400

    added button

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,22 +5,20 @@ import { Container,Navbar } from 'react-bootstrap';
 const Nav = () => {
   return (
     <div>
-        
-<Navbar className='nav'>
-  <Container className='navhome'>
-    <Navbar.Brand><Link to="/">Home</Link></Navbar.Brand>
-    <Navbar.Toggle />
-    <Navbar.Collapse className="justify-content-end"> 
-      <Navbar.Text>
-          <ul className='links'>
-            <li> <Link to="/Signin">Signin</Link></li>
-            <li> <Link to="/Signout">Signout</Link></li>
-            
-          </ul>
-      </Navbar.Text>
-    </Navbar.Collapse>
-  </Container>
-</Navbar>
+        <Navbar className='nav'>
+        <Container className='navhome'>
+            <Navbar.Brand><Link to="/">Home</Link></Navbar.Brand>
+            <Navbar.Toggle />
+            <Navbar.Collapse className="justify-content-end"> 
+            <Navbar.Text>
+                <ul className='links'>
+                    <li> <Link to="/Signin">Signin</Link></li>
+                    <li> <Link to="/Signout">Signout</Link></li>    
+                </ul>
+            </Navbar.Text>
+            </Navbar.Collapse>
+        </Container>
+        </Navbar>
        
     </div>
   )
--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import GoogleButton from 'react-google-button'
+// import FacebookLogin from 'react-facebook-login';
 
 export const Signin = () => {
   return (
-    <div>Signin</div>
+    <div className='gbutton'>
+        <GoogleButton />
+        {/* <FacebookLogin/> */}
+    </div>
   )
 }
