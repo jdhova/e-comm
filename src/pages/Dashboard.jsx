9463197ceed3cb94e4de7f892f7ca0b1abcc7535
Jude Okagu <juud0010@gmail.com>
2022-07-10 12:57:13 -0400

    adding fields

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -55,6 +55,56 @@ const Dashboard = () => {
         <br></br>
          {user.displayName}  its {today}{check()}
     <Button onClick={handleSignOut}>Logout</Button>
+
+    <>
+    <div className="form-group">
+        <label htmlFor="inputAddress">Address</label>
+        <input
+        type="text"
+        className="form-control"
+        id="inputAddress"
+        placeholder="1234 Main St"
+        />
+        </div>
+        <div className="form-group">
+            <label htmlFor="inputAddress2">Address 2</label>
+            <input
+            type="text"
+            className="form-control"
+            id="inputAddress2"
+            placeholder="Apartment, studio, or floor"
+            />
+        </div>
+        <div className="form-row">
+            <div className="form-group col-md-6">
+            <label htmlFor="inputCity">City</label>
+            <input type="text" className="form-control" id="inputCity" />
+            </div>
+            <div className="form-group col-md-4">
+            <label htmlFor="inputState">State</label>
+            <select id="inputState" className="form-control">
+                <option selected="">Choose...</option>
+                <option>...</option>
+            </select>
+            </div>
+            <div className="form-group col-md-2">
+            <label htmlFor="inputZip">Zip</label>
+            <input type="text" className="form-control" id="inputZip" />
+            </div>
+        </div>
+        <div className="form-group">
+            <div className="form-check">
+            <input className="form-check-input" type="checkbox" id="gridCheck" />
+            <label className="form-check-label" htmlFor="gridCheck">
+                Check me out
+            </label>
+            </div>
+        </div>
+        <button type="submit" className="btn btn-primary">
+            Submit
+        </button>
+</>
+
     </>
   )
 }
